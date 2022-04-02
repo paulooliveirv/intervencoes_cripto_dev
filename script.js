84493627744786e9f75d7e32449c2d160e978846
Paulo Henrique Reis <87020059+paulooliveirv@users.noreply.github.com>
2022-04-02 12:11:33 -0300

    atividade finalizada

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt('Olá, digite seu nome:  ');
+let idade = prompt('Agora, qual a sua idade?');
+let email = prompt('Digite agora o seu email');
+
+document.write(`Olá ${nome},  você tem ${idade} certo? Verifiquei também que o seu email é: ${email}`);
